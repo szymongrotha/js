05988b73cdd13edf6114041d798cc6b3fffd0dc9
Szymon Grotha <szymon.grotha@gmail.com>
2016-11-05 16:01:41 +0100

    First conditional statmets task Kodilla

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+{
+	var a = prompt('Podaj wartość a');
+	var b = prompt('Podaj wartość b');
+	var value = (a*a) + (2 * a * b) - (b*b);
+
+	console.log('Wynik działania to ' +value);
+
+}	
+
+	if (value < 0) {
+		console.log('Wynik jest ujemny');	
+	}
+
+	else if (value > 0) {
+		console.log ('Wynik jest dodatni');
+	}
+
+	else {
+		console.log ('Wynik to zero');
+	}
+
